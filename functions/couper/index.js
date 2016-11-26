54549d6de9c8b1b90c39e5afbd5e2302797111f2
Johannes Boyne <johannes.boyne@bcgdv.com>
2016-11-26 14:39:17 +0100

    Reuse refpoint

--- functions/couper/index.js
+++ functions/couper/index.js
@@ -12,16 +12,18 @@ exports.handle = function(e, ctx, cb) {
     res.on('end', _ => {
       // process json
       const scooters    = JSON.parse(json)
+      // office location
       const refPoint    = {type: 'Point', coordinates:[13.4074892, 52.5233866]};
       const pin         = 'pin-m-bicycle+8cc'
       const mapstyle    = 'mapbox.streets'
       const mapboxURL1  = 'https://api.tiles.mapbox.com/v4/'+mapstyle+'/'
-      let mapboxURLk    = 'pin-m-star+88c(13.4074892,52.5233866),'
+      let mapboxURLk    = 'pin-m-star+88c(' + refPoint.coordinates.join(',') + '),'
       let mapboxURLg    = '/' + refPoint.coordinates.join(',') + ',16'
       const mapboxURLn  = '/800x800@2x.png?access_token=' + process.env.MAPBOX_TOKEN
 
       // map, sort, slice scooters
       scooters.data.scooters = scooters.data.scooters.map(s => {
+        // calculate the distance to our refPoint
         s.distance = gju.pointDistance({type: 'Point', coordinates:[s.location.lng, s.location.lat]}, refPoint)
         return s
       }).sort((a,b) => {
